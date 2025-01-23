b92e6b6a51574500c7be26e0f6dba3da7b5c262e
Eliphaz Bouye <eliphazbouye@gmail.com>
2025-01-23 01:08:15 +0000

    feat(): neovim blade treesitter configuration

--- .config/nvim/lua/core/config/treesitter-config.lua
+++ .config/nvim/lua/core/config/treesitter-config.lua
@@ -3,11 +3,21 @@ local M = {}
 function M.init ()
   local configs = require("nvim-treesitter.configs")
 
+  local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+  parser_config.blade = {
+    install_info = {
+      url = "~/.config/nvim/lua/tree-sitter-blade",
+      files = { "src/parser.c" }
+    },
+    filetype = "phtml",
+  }
+
   configs.setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "elixir", "javascript", "typescript", "html", "css", "python" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "elixir", "javascript", "typescript", "html", "css", "python", "blade" },
     sync_install = false,
     highlight = { enable = true },
-    indent = { enable = true },  
+    indent = { enable = true },
   })
 end
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,17 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    require("core.config.treesitter-config").init {}
-  end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+    },
+    config = function ()
+      require("core.config.treesitter-config").init {}
+      vim.treesitter.language.register("blade", "blade")
+    end
+  },
+  {
+    "EmranMR/tree-sitter-blade",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = "blade"
+  }
 }
